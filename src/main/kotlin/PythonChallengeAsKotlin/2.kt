cb27f09e8642054de00daf8c33980c709f23a98e
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-10-26 09:06:58 -0300

    2.kt - Wrote the basic idea. Trying to come up with how to solve it

--- src/main/kotlin/PythonChallengeAsKotlin/2.kt
+++ src/main/kotlin/PythonChallengeAsKotlin/2.kt
@@ -1,2 +1,11 @@
 package PythonChallengeAsKotlin
 
+// http://www.pythonchallenge.com/pc/def/map.html
+fun main() {
+    var todecode :String = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj. "
+
+
+    // So initially we have a string and the image showing.: k -> m, o -> q, e -> g.
+    // This is def a ceaser cypher, with a shift of x. I'd say that, considering the ascii table, k = 107 and m = 109,
+    // that would mean x = 109-107?
+}
