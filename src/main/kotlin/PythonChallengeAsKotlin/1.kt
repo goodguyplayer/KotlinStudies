790e575585cee1acdc6c51225429bfa5b5a35585
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-10-26 08:44:16 -0300

    Solved PythonChallenge 1 with 1.kt

--- src/main/kotlin/PythonChallengeAsKotlin/1.kt
+++ src/main/kotlin/PythonChallengeAsKotlin/1.kt
@@ -1,5 +1,13 @@
 package PythonChallengeAsKotlin
 
+import java.lang.Math.pow
+
+// Going to try and use http://www.pythonchallenge.com/ to practice some initial bits
 fun main() {
-    
+    var base :Int = 2
+    var expo :Int = 38
+    var result :Double = pow(base.toDouble(), expo.toDouble())
+    println(result)
+
+
 }
