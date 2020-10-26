51a3e740630851bdbf5abb0be65490eee19197a3
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-10-26 09:34:19 -0300

    2.kt - Updated with the character swapper and it works
    Now the interesthing thing would be to exclude chars that aren't letters

--- src/main/kotlin/PythonChallengeAsKotlin/2.kt
+++ src/main/kotlin/PythonChallengeAsKotlin/2.kt
@@ -3,18 +3,26 @@ package PythonChallengeAsKotlin
 // http://www.pythonchallenge.com/pc/def/map.html
 fun main() {
     var todecode :String = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj. "
-    println(swapper(todecode, 3))
+
 
     // So initially we have a string and the image showing.: k -> m, o -> q, e -> g.
     // This is def a ceaser cypher, with a shift of x. I'd say that, considering the ascii table, k = 107 and m = 109,
     // that would mean x = 109-107?
+    println(swapper(todecode, 2))
 }
 
 fun swapper(todecode: String, shift: Int): String {
     var output :String = ""
 
     todecode.forEach { c: Char ->
-        output += c
+        output += (c.toByte().toInt() + shift).toChar()
     }
     return output
 }
+
+/*
+Stackoverflow links used.:
+
+https://stackoverflow.com/questions/47234812/how-to-convert-char-to-ascii-value-in-kotlin-language
+
+ */
