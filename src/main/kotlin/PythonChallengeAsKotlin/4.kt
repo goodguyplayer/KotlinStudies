f675df9c485a6568b779250ff2cf4e7692391f72
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-10-28 09:40:39 -0300

    4.kt updated and moving to the next

--- src/main/kotlin/PythonChallengeAsKotlin/4.kt
+++ src/main/kotlin/PythonChallengeAsKotlin/4.kt
@@ -9,6 +9,12 @@ import java.io.File
 AAAbAAA. Would be best to use regex for this, filter all occurrences and print string
  */
 
+// ... Regex can be annoying at times.
 fun main() {
-    readFile("src/for4.txt")
+    var text = readFile("src/for4.txt")
+    val reg = """[^A-Z]+[A-Z]{3}([a-z])[A-Z]{3}[^A-Z]+""".toRegex()
+    val matchResults = reg.findAll(text)
+    for (char in matchResults){
+        println(char.value)
+    }
 }
