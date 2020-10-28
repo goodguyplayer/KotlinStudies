f1463886f5cc005b5fd6bf02949023880fa80604
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-10-28 09:52:57 -0300

    4.kt started

--- src/main/kotlin/PythonChallengeAsKotlin/4.kt
+++ src/main/kotlin/PythonChallengeAsKotlin/4.kt
@@ -1,2 +1,16 @@
 package PythonChallengeAsKotlin
 
+// http://www.pythonchallenge.com/pc/def/linkedlist.php
+
+/*
+"And the next nothing is (number)"
+Need a way to match this precise string in a different string. Then for each page, change
+http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=nextNothing
+
+ */
+
+
+fun main() {
+    val link: String = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="
+    var nothing: String = "12345"
+}
