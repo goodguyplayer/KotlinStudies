62dbf3cb2736a330d1e89a5815893ab1c76e7fd5
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-10-28 10:00:40 -0300

    HttpRequests.kt updated and working

--- src/main/kotlin/PythonChallengeAsKotlin/HttpRequests.kt
+++ src/main/kotlin/PythonChallengeAsKotlin/HttpRequests.kt
@@ -1,2 +1,15 @@
 package PythonChallengeAsKotlin
 
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+fun httpRequest(urllink: String): HttpResponse<String>? {
+    val client = HttpClient.newBuilder().build();
+    val request = HttpRequest.newBuilder()
+            .uri(URI.create(urllink))
+            .build();
+    val response = client.send(request, HttpResponse.BodyHandlers.ofString());
+    return response;
+}
