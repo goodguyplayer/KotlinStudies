a6c71ec40b1cd25c729c58451a38aa1ba7a78b0a
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-10-28 10:10:36 -0300

    4.kt - Initial testing to get the right regex and the number

--- src/main/kotlin/PythonChallengeAsKotlin/4.kt
+++ src/main/kotlin/PythonChallengeAsKotlin/4.kt
@@ -13,4 +13,10 @@ http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=nextNothing
 fun main() {
     val link: String = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="
     var nothing: String = "12345"
+    val regex = """and the next nothing is (\d+)""".toRegex()
+    val regex2 = """(\d+)""".toRegex()
+
+    var matchResult = httpRequest(link+nothing)?.body()?.let { regex.find(it) }
+    var next = matchResult!!.destructured
+    println(regex2.find(matchResult.value)?.value)
 }
