6804a3ffb7839091acdefb561bbc3cfbb1fc34eb
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-10-28 09:06:00 -0300

    3.kt updated

--- src/main/kotlin/PythonChallengeAsKotlin/3.kt
+++ src/main/kotlin/PythonChallengeAsKotlin/3.kt
@@ -2,6 +2,27 @@ package PythonChallengeAsKotlin
 
 // http://www.pythonchallenge.com/pc/def/ocr.html
 
+/*
+Here's what we'll do.
+    - Load file with the text in it.
+    - Create a list that will take all possible characters
+    - For every character found in it, count how many of those there are. Store in list, then remove the character from string
+    - Considering we're meant to "find rare characters", will make list print only those characters that shows 40 times or less.
+
+    - Rather, we don't /need/ to save the characters. We can just count and pop them out.
+ */
 fun main() {
     
+}
+
+
+// To read file and return the whole string
+fun readFile(){
+
+}
+
+
+// To read each character and pop them out of the string
+fun countPop(){
+
 }
