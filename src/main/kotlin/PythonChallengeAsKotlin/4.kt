1bfa2bda44975762653a345e3c3c82eef8064b8c
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-10-28 10:59:40 -0300

    4.kt updated -- Moving to 6th challenge due to pickle

--- src/main/kotlin/PythonChallengeAsKotlin/4.kt
+++ src/main/kotlin/PythonChallengeAsKotlin/4.kt
@@ -1,5 +1,9 @@
 package PythonChallengeAsKotlin
 
+import java.lang.Exception
+import java.util.*
+import kotlin.concurrent.schedule
+
 // http://www.pythonchallenge.com/pc/def/linkedlist.php
 
 /*
@@ -12,11 +16,29 @@ http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=nextNothing
 
 fun main() {
     val link: String = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="
-    var nothing: String = "12345"
+    //var nothing: String = "12345" -- Last interrupt stopped at 23053
+    //var nothing: String = "16044"
+    var nothing: String = (16044/2).toString()  // Stops at 66831
     val regex = """and the next nothing is (\d+)""".toRegex()
     val regex2 = """(\d+)""".toRegex()
 
-    var matchResult = httpRequest(link+nothing)?.body()?.let { regex.find(it) }
-    var next = matchResult!!.destructured
-    println(regex2.find(matchResult.value)?.value)
+    var matchResult = httpRequest(link+nothing)?.body()
+    var regexFind = matchResult?.let { regex.find(it) }
+
+
+    try {
+        while (true){
+            matchResult = httpRequest(link+nothing)?.body()
+            regexFind = matchResult?.let { regex.find(it) }
+            nothing = (regexFind?.value?.let { regex2.find(it)?.value }).toString()
+            println(matchResult)
+            println(nothing)
+        }
+    }catch (e: Exception){
+        e.printStackTrace()
+    }
+
+
+
 }
+
